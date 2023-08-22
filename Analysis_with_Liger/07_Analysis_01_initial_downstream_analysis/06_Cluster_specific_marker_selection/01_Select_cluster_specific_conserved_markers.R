49de58822849f9d192d299185e687b4044d5608b
Yasir <tamalarafat@gmail.com>
2023-08-22 15:47:39 +0200

    Added cluster specific conserved markers and cluster specific markers for each cluster

---
+++ Analysis_with_Liger/07_Analysis_01_initial_downstream_analysis/06_Cluster_specific_marker_selection/01_Select_cluster_specific_conserved_markers.R
@@ -0,0 +1,26 @@
+# Load all the functions stored in scripts from the folder housing the scripts
+scripts_list <- list.files("/home/yasir/Documents/Projects_Yasir/scExplorer/Functions", pattern = "*.R$", full.names = TRUE)
+sapply(scripts_list, source, .GlobalEnv)
+
+## Define the colors
+grp_col = c("#FD6467", "#00A08A", "#F98400", "#046C9A", "#075149FF", "#FFA319FF", "#00BF81", "#767676FF", "#FD8CC1FF")
+
+# Markers from cluster DEGs
+# Dir - containing the DEG files
+DEG_dir = "/home/yasir/Documents/Projects_Yasir/comparative_study_of_wt_and_mutant_genotype_Cardamine/Analysis_with_Liger/07_Analysis_01_initial_downstream_analysis/Analysis_outputs/Differentially_expressed_genes/Conserved_marker_grouped_by_Genotypes/Conserved_markers_DEtest_wilcox"
+
+# Storing directory
+storing_dir = "/home/yasir/Documents/Projects_Yasir/comparative_study_of_wt_and_mutant_genotype_Cardamine/Analysis_with_Liger/07_Analysis_01_initial_downstream_analysis/Analysis_outputs"
+
+specific_markers_list = specific_conserved_marker_finder(DEG_file = DEG_dir, 
+                                                         max.pct2.detection = 0.1, 
+                                                         include.pct.difference = TRUE, 
+                                                         pct.difference = 0.3, 
+                                                         store.marker.file = TRUE, 
+                                                         store_dir = storing_dir, 
+                                                         store_folder = "Cluster_specific_conserved_markers")
+
+
+
+
+
---
+++ Analysis_with_Liger/07_Analysis_01_initial_downstream_analysis/06_Cluster_specific_marker_selection/02_Select_cluster_specific_markers.R
@@ -0,0 +1,15 @@
+# Load all the functions stored in scripts from the folder housing the scripts
+scripts_list <- list.files("/home/yasir/Documents/Projects_Yasir/scExplorer/Functions", pattern = "*.R$", full.names = TRUE)
+sapply(scripts_list, source, .GlobalEnv)
+
+## Define the colors
+grp_col = c("#FD6467", "#00A08A", "#F98400", "#046C9A", "#075149FF", "#FFA319FF", "#00BF81", "#767676FF", "#FD8CC1FF")
+
+# Markers from cluster DEGs
+# Dir - containing the DEG files
+DEG_dir = "/home/yasir/Documents/Projects_Yasir/comparative_study_of_wt_and_mutant_genotype_Cardamine/Analysis_with_Liger/07_Analysis_01_initial_downstream_analysis/Analysis_outputs/Differentially_expressed_genes/DEGs_of_each_cluster/DEG_DEtest_wilcox"
+
+# Storing directory
+storing_dir = "/home/yasir/Documents/Projects_Yasir/comparative_study_of_wt_and_mutant_genotype_Cardamine/Analysis_with_Liger/07_Analysis_01_initial_downstream_analysis/Analysis_outputs"
+
+specific_markers_list = specific_marker_finder(DEG_file_dir = DEG_dir, pct_detection = 0.1, pct_diff = 0.3, store_dir = storing_dir)
